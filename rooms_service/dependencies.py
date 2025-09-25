64a6cd8d3c0ba3ea833d5efd35e652ffc30f9755
AndreyKosov <andrey.kosov.05@inbox.ru>
2025-09-25 22:19:24 +0500

    25.09.2025 Оптимизирован IOC контейнер

--- rooms_service/dependencies.py
+++ rooms_service/dependencies.py
@@ -3,23 +3,17 @@ from typing import Final
 from collections.abc import AsyncIterator
 
 from dishka import AsyncContainer, Provider, Scope, from_context, make_async_container, provide
-from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker
+from sqlalchemy.ext.asyncio import AsyncSession
 
-from .database.base import create_sessionmaker
+from .database.base import sessionmaker
 from .settings import Settings, settings
 
 
 class AppProvider(Provider):
     app_settings = from_context(provides=Settings, scope=Scope.APP)
 
-    @provide(scope=Scope.APP)
-    def get_sessionmaker(self, app_settings: Settings) -> async_sessionmaker[AsyncSession]:  # noqa: PLR6301
-        return create_sessionmaker(app_settings.postgres.sqlalchemy_url)
-
     @provide(scope=Scope.REQUEST)
-    async def get_session(  # noqa: PLR6301
-            self, sessionmaker: async_sessionmaker[AsyncSession]
-    ) -> AsyncIterator[AsyncSession]:
+    async def get_session() -> AsyncIterator[AsyncSession]:
         async with sessionmaker() as session:
             yield session
 
